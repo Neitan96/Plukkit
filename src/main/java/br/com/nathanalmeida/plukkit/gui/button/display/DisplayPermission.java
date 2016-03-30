576923adce908350b77d4c94d959a8e041d7d463
Nathan Almeida <neitan96@outlook.com>
2016-03-30 16:27:25 -0300

    GUIDisplays padrões adicionados

---
+++ src/main/java/br/com/nathanalmeida/plukkit/gui/button/display/DisplayPermission.java
@@ -0,0 +1,40 @@
+package br.com.nathanalmeida.plukkit.gui.button.display;
+
+import br.com.nathanalmeida.plukkit.gui.button.GUIButton;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Project: Plukkit
+ * Author: Neitan96
+ * Since: 30/03/2016 13:54
+ */
+public class DisplayPermission implements GUIDisplay{
+
+    protected final GUIDisplay display;
+    protected final String[] permissions;
+
+    public DisplayPermission(GUIDisplay display, String... permissions){
+        this.display = display;
+        this.permissions = permissions;
+    }
+
+    public boolean hasPermission(Player player){
+        if(permissions == null || permissions.length == 0)
+            return true;
+
+        for(String permission : permissions){
+            if(player.hasPermission(permission))
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public ItemStack renderDisplay(Player player, GUIButton button){
+        if(hasPermission(player))
+            return display.renderDisplay(player, button);
+        return null;
+    }
+}
---
+++ src/main/java/br/com/nathanalmeida/plukkit/gui/button/display/DisplaysPermissions.java
@@ -0,0 +1,29 @@
+package br.com.nathanalmeida.plukkit.gui.button.display;
+
+import br.com.nathanalmeida.plukkit.gui.button.GUIButton;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Project: Plukkit
+ * Author: Neitan96
+ * Since: 30/03/2016 15:32
+ */
+public class DisplaysPermissions implements GUIDisplay{
+
+    protected final DisplayPermission[] displays;
+
+    public DisplaysPermissions(DisplayPermission... displays){
+        this.displays = displays;
+    }
+
+    @Override
+    public ItemStack renderDisplay(Player player, GUIButton button){
+        for(DisplayPermission display : displays){
+            ItemStack itemStack = display.renderDisplay(player, button);
+            if(itemStack != null) return itemStack;
+        }
+        return null;
+    }
+
+}
