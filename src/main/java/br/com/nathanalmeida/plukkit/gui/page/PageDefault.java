8e865f8932ca61f07f651c2c5cacaed0e28f6df8
Nathan Almeida <neitan96@outlook.com>
2016-03-31 02:34:39 -0300

    Adicionado display padrão no GUIPage padrão

--- src/main/java/br/com/nathanalmeida/plukkit/gui/page/PageDefault.java
+++ src/main/java/br/com/nathanalmeida/plukkit/gui/page/PageDefault.java
@@ -6,6 +6,7 @@ import br.com.nathanalmeida.plukkit.gui.holder.GUIInventoryHolder;
 import br.com.nathanalmeida.plukkit.gui.manager.GUIManager;
 import br.com.nathanalmeida.plukkit.gui.page.title.GUITitle;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -26,20 +27,23 @@ public class PageDefault implements GUIPage{
     protected final GUITitle title;
     protected final int size;
     protected final String permission;
+    protected final ItemStack displayDefault;
 
     protected final List<GUIButton> buttons = new ArrayList<>();
 
-    public PageDefault(GUIManager manager, String name, GUITitle title, int size, String permission){
+    public PageDefault(GUIManager manager, String name, GUITitle title,
+                       int size, String permission, ItemStack displayDefault){
         this.manager = manager;
         this.name = name;
         this.title = title;
         this.size = size;
         this.permission = permission;
+        this.displayDefault = displayDefault;
     }
 
     public PageDefault(GUIManager manager, String name, GUITitle title,
-                       int size, String permission, GUIButton... buttons){
-        this(manager, name, title, size, permission);
+                       int size, String permission, ItemStack displayDefault, GUIButton... buttons){
+        this(manager, name, title, size, permission, displayDefault);
         Collections.addAll(this.buttons, buttons);
     }
 
@@ -81,6 +85,14 @@ public class PageDefault implements GUIPage{
             ItemStack display = button.makeDisplay(player);
             if(display != null)
                 inventory.setItem(button.getSlot(), display);
+        }
+
+        if(displayDefault != null){
+            for(int i = 0; i < inventory.getMaxStackSize(); i++){
+                ItemStack item = inventory.getItem(i);
+                if(item == null || item.getType() == Material.AIR)
+                    inventory.setItem(i, displayDefault);
+            }
         }
 
         return inventory;
