fedec37a6359e3ff60b790864c1ef88b47b452c6
Nathan Almeida <neitan96@outlook.com>
2016-04-05 20:25:04 -0300

    Adicionado base para validação de comandos

---
+++ src/br/com/nathanalmeida/plukkit/commands/validator/CmdValidators.java
@@ -0,0 +1,87 @@
+package br.com.nathanalmeida.plukkit.commands.validator;
+
+import br.com.nathanalmeida.plukkit.commands.arguments.CommandArguments;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Project: Plukkit
+ * Author: Neitan96
+ * Since: 05/04/2016 20:11
+ */
+public final class CmdValidators{
+
+    public static CommandValidator argsMin(final int min){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return arguments.length() >= min;
+            }
+        };
+    }
+
+    public static CommandValidator argsMax(final int max){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return arguments.length() <= max;
+            }
+        };
+    }
+
+    public static CommandValidator argsRange(final int min, final int max){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return arguments.length() >= min && arguments.length() <= max;
+            }
+        };
+    }
+
+    public static CommandValidator senderType(final Class typeSender){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return sender.getClass().isAssignableFrom(typeSender);
+            }
+        };
+    }
+
+    public static CommandValidator senderIsPlayer(){
+        return senderType(Player.class);
+    }
+
+    public static CommandValidator senderIsConsole(){
+        return senderType(ConsoleCommandSender.class);
+    }
+
+    public static CommandValidator senderIsOP(){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return sender.isOp();
+            }
+        };
+    }
+
+    public static CommandValidator senderNotOP(){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return !sender.isOp();
+            }
+        };
+    }
+
+    public static CommandValidator permission(final String permission){
+        return new CommandValidator(){
+            @Override
+            public boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments){
+                return sender.hasPermission(permission);
+            }
+        };
+    }
+
+}
---
+++ src/br/com/nathanalmeida/plukkit/commands/validator/CommandValidator.java
@@ -0,0 +1,16 @@
+package br.com.nathanalmeida.plukkit.commands.validator;
+
+import br.com.nathanalmeida.plukkit.commands.arguments.CommandArguments;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Project: Plukkit
+ * Author: Neitan96
+ * Since: 05/04/2016 19:50
+ */
+public interface CommandValidator{
+
+    boolean validCommand(CommandSender sender, Command command, String label, CommandArguments arguments);
+
+}
