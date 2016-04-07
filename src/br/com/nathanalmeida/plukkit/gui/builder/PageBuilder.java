5356854c42cddf090f7a64254a38eec8f0926bd8
Nathan Almeida <neitan96@outlook.com>
2016-04-07 02:58:28 -0300

    Adicionado builders de page de GUI

---
+++ src/br/com/nathanalmeida/plukkit/gui/builder/PageBuilder.java
@@ -0,0 +1,95 @@
+package br.com.nathanalmeida.plukkit.gui.builder;
+
+import br.com.nathanalmeida.plukkit.gui.core.button.GUIButton;
+import br.com.nathanalmeida.plukkit.gui.core.manager.GUIManager;
+import br.com.nathanalmeida.plukkit.gui.core.page.PageDefault;
+import br.com.nathanalmeida.plukkit.gui.core.page.title.GUITitle;
+import br.com.nathanalmeida.plukkit.gui.core.page.title.TitleDefault;
+import br.com.nathanalmeida.plukkit.gui.core.page.title.TitleList;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project: Plukkit
+ * Author: Neitan96
+ * Since: 07/04/2016 00:43
+ */
+public class PageBuilder implements ButtonAdder{
+
+    protected final GUIManager manager;
+    protected final PageAdder adder;
+    protected final String name;
+
+    protected List<GUITitle> titles = new ArrayList<>();
+    protected List<GUIButton> buttons = new ArrayList<>();
+    protected int size = -1;
+    protected String permission;
+    protected ItemStack itemDefault;
+
+
+    public PageBuilder(GUIManager manager, PageAdder adder, String name){
+        this.manager = manager;
+        this.adder = adder;
+        this.name = name;
+    }
+
+
+    public PageBuilder addTitle(GUITitle title){
+        titles.add(title);
+        return this;
+    }
+
+    public PageBuilder addTitle(String title, String permission){
+        return addTitle(new TitleDefault(title, manager.getBinder(), permission));
+    }
+
+    public PageBuilder addTitle(String title){
+        return addTitle(title, null);
+    }
+
+
+    public PageBuilder setSize(int size){
+        this.size = size;
+        return this;
+    }
+
+
+    public PageBuilder setPermission(String permission){
+        this.permission = permission;
+        return this;
+    }
+
+
+    public PageBuilder setItemDefault(ItemStack itemDefault){
+        this.itemDefault = itemDefault;
+        return this;
+    }
+
+    @Override
+    public void addButton(GUIButton button){
+        this.buttons.add(button);
+    }
+
+    public ButtonBuilder addButton(){
+        return new ButtonBuilder(manager, this);
+    }
+
+
+    public PageBuilder buildPage(){
+        GUITitle title = null;
+        if(titles.size() == 1){
+            title = titles.get(0);
+
+        }else if(titles.size() > 0){
+            GUITitle[] titles = this.titles.toArray(new GUITitle[this.titles.size()]);
+            title = new TitleList(titles);
+        }
+
+        PageDefault page = new PageDefault(name, title, size, permission, itemDefault);
+        adder.addPage(page);
+
+        return this;
+    }
+}
